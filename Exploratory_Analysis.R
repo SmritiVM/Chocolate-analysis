cefa8cf5dcb52ab7ca7db4d51c2310e68d8608ef
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-03-25 23:16:29 +0530

    Random failed stuff

--- Exploratory_Analysis.R
+++ Exploratory_Analysis.R
@@ -11,18 +11,39 @@ column_names
 library(dplyr)
 chocolate <- select(chocolate, -c(...1,ref,fourth_taste))
 
+chocolate <- data.frame(chocolate)
+
 #Modifying the columns
 #Changing have/have not to 1/0
+
+chocolate = data.frame(chocolate)
+colnames(chocolate)[9] <- "bean"
+colnames(chocolate)[11] <- "vanila"
 
 #Columns to be changed: 9 to 15
 bool_columns = colnames(chocolate[9:15])
 bool_columns
 
-for (column in bool_columns){
-  have_not <- paste("have_not_", column, sep = "")
-  replace(chocolate[column], have_not, 0)
+<<<<<<< HEAD
+chocolate$beans
+for (x in 1:7){
+  have_not <- paste("have_not_", bool_columns[x], sep = "")
+  have <- paste("have_", bool_columns[x], sep = "")
   
-  #have <- paste("have_", column, sep = "")
+  print(have_not)
+  #print(x)
+  #print(chocolate[x + 8])
+  chocolate[x + 8] <- replace(chocolate[x + 8], c(have_not, have), c(0,1))
+  
   #chocolate[column] <- replace(chocolate[column], have, 1)
   
 }
+=======
+for (column in bool_columns){
+  have_not <- paste("have_not_", column, sep = "")
+  chocolate[column] = replace(chocolate[column], chocolate[column] == have_not, 0);
+
+  have <- paste("have_", column, sep = "")
+  chocolate[column] <- replace(chocolate[column], chocolate[column] == have, 1)
+}
+>>>>>>> 3623f9ba576790eff0c6337e909d8f2033a59fd6
