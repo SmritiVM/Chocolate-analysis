5ffab471ab2d73212ccbe97b2c1e1f363c850535
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-03-25 22:12:38 +0530

    Column names

--- Exploratory_Analysis.R
+++ Exploratory_Analysis.R
@@ -3,4 +3,8 @@ library(readr) #to read the csv file
 chocolate <- read_csv("chocolate.csv")
 View(chocolate)
 
+#View column names
+column_names = colnames(chocolate)
+column_names
+
 library(dplyr)
