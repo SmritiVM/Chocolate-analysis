b4d32e4ebef5f087bae31b46fc1e29f496c77bd1
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-03-30 15:23:27 +0530

    Create Chocolate_Analysis.R

---
+++ Chocolate_Analysis.R
@@ -0,0 +1,15 @@
+#Questions
+#1. Multiple regression (model rating in terms of cocoa percent and count of ingredients)
+
+
+
+#2. Hypothesis testing (take top 10 companies producing chocolates as a sample and see if it's representative of the entire population) mean of the rating
+
+
+
+#3. Hypothesis testing (take 2 countries, compare the average reviews) - check of one country produces objectively better chocolates 
+
+
+
+
+#4. Multiple correlation (salt, sugar, cocoa_butter, vanilla) 
