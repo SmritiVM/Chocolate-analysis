f04da0a317ec02d3dadd373e08731bb3ebed5a84
hiteshsd04 <91045293+hiteshsd04@users.noreply.github.com>
2023-04-09 20:10:52 +0530

    Update Chocolate_Analysis.R

--- R Files/Chocolate_Analysis.R
+++ R Files/Chocolate_Analysis.R
@@ -55,7 +55,83 @@ scatterplot3d(Rating_data$avg_rating ~ Rating_data$avg_cocoa_percent + Rating_da
               zlab = "Rating")
 
 #3. Hypothesis testing (take top 10 companies producing chocolates as a sample and see if it's representative of the entire population) mean of the rating
+sample_mean=0
+sample_size=0
+temp = data.frame(sort(table(chocolate$company), decreasing=TRUE)[1:10])
+temp
 
+soma = data.frame(chocolate[chocolate$company=='Soma' , ])
+tempmean=mean(soma$rating)
+n=nrow(soma)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+arete = data.frame(chocolate[chocolate$company=='Arete' , ])
+tempmean=mean(arete$rating)
+n=nrow(arete)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+fresco = data.frame(chocolate[chocolate$company=='Fresco' , ])
+tempmean=mean(fresco$rating)
+n=nrow(fresco)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+bonnat = data.frame(chocolate[chocolate$company=='Bonnat' , ])
+tempmean=mean(bonnat$rating)
+n=nrow(bonnat)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+pralus = data.frame(chocolate[chocolate$company=='Pralus' , ])
+tempmean=mean(pralus$rating)
+n=nrow(pralus)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+amorin = data.frame(chocolate[chocolate$company=='A. Morin' , ])
+tempmean=mean(amorin$rating)
+n=nrow(amorin)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+domori = data.frame(chocolate[chocolate$company=='Domori' , ])
+tempmean=mean(domori$rating)
+n=nrow(domori)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+guittard = data.frame(chocolate[chocolate$company=='Guittard' , ])
+tempmean=mean(guittard$rating)
+n=nrow(guittard)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+valrhona = data.frame(chocolate[chocolate$company=='Valrhona' , ])
+tempmean=mean(valrhona$rating)
+n=nrow(valrhona)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+zotter = data.frame(chocolate[chocolate$company=='Zotter' , ])
+tempmean=mean(zotter$rating)
+n=nrow(zotter)
+sample_size=sample_size+n
+sample_mean=sample_mean+tempmean
+
+sample_mean=sample_mean/10
+population_mean=mean(chocolate$rating)
+sigma=sd(chocolate$rating)
+
+z=(sample_mean-population_mean)/(sigma/sqrt(sample_size))
+
+alpha=0.05
+zhalfalpha=qnorm(1-(alpha/2))
+c(-zhalfalpha,zhalfalpha)
+pval=2*pnorm(z)
+
+if(pval>alpha){print("Accept Null hypothesis")} else{print("Reject Null hypothesis")}
 
 
 #4. Hypothesis testing (take 2 countries, compare the average reviews) - check of one country produces objectively better chocolates 
